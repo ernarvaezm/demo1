a698f1c28ec0e1c991988e97693ed8ba65a18f3c
Eliecer Narvaez <eliecer.narvaez@ibm.com>
2022-11-23 12:08:15 -0600

    sgs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+
+    const meta = document.querySelector('meta[name=theme-color]')
+    const color = getComputedStyle(document.documentElement).getPropertyValue('--color-logo');
+  }, []);
+
+  const sayHello = (name) => {
+
+
+    const meta = document.querySelector('meta[name=theme-color]')
+    const color1 = getComputedStyle(document.documentElement).getPropertyValue('--color1');
+    const color2 = getComputedStyle(document.documentElement).getPropertyValue('--color2');
+    const color3 = getComputedStyle(document.documentElement).getPropertyValue('--color3');
+
+    let value5 = Math.floor(Math.random() * 4);
+
+    switch (value5) {
+      case 1:
+        meta.setAttribute('content', color1)
+        break;
+
+      case 2:
+        meta.setAttribute('content', color2)
+        break;
+      case 4:
+        meta.setAttribute('content', color3)
+        break;
+      default:
+        meta.setAttribute('content', 'black')
+        break;
+    }
+
+    console.log(meta);
+    console.log(value5);
+
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +58,18 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div className="demo">
+        <button
+          onClick={() => {
+            sayHello("Yomi");
+          }}
+        >
+          Say Hello
+        </button>
+
+      </div>
+
     </div>
   );
 }
